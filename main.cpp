3c1076a8848215b6006332401023d222d0324395
Alexandr <a.kopnov24@gmail.com>
2023-05-02 13:32:09 +0300

    does not work, do not care

--- main.cpp
+++ main.cpp
@@ -1,8 +1,133 @@
 #include <iostream>
+#include <functional>
+#include "simplex.h"
+#include <sstream>
 
 using namespace std;
 
+std::tuple<task_t, set<int>, set<int>> read(string filename);
+vector<string> split(string line, char sep);
+vector_t restore(vector_t& xCan, int size, set<int>& unbound);
+
+double f(vector_t x) { return x[0] * x[0] + 4 * x[1] * x[1] + sin(6 * x[0] + 7 * x[1]) + 3 * x[0] + 2 * x[1]; }
+
+vector_t grad(vector_t x) {
+    vector_t res(3);
+    res[0] = 2 * x[0] + 6 * cos(6 * x[0] + 7 * x[1]) + 3;
+    res[1] = 8 * x[1] + 7 * cos(6 * x[0] + 7 * x[1]) + 2;
+    res[2] = 0;
+    return res;
+}
+
 int main(int argc, char* argv[]) {
-    cout << "Hello world\n";
+    auto [task, unbound, noneq] = read("task.csv");
+    task_t primCan = genCanon(task,unbound,noneq,extrem::min);
+
+    auto init = initBasic(primCan);
+    auto x0 = simplex(primCan,init,{});
+    auto x0r = restore(x0, task.C.size(), unbound);
+    // auto [dual, dUnbound, dNoneq] = genDual(task, unbound, noneq);
+    // auto dualCan = genCanon(dual,dUnbound,dNoneq,extrem::max);
+    // dualCan.C = dualCan.C *-1;
+    // cout << dualCan.C.T() << endl << dualCan.A << endl;
+    // auto dInit = initBasic(dualCan);
+    // auto dualCanSol = simplex(dualCan,dInit,{});
+    // cout << dualCanSol.T();
+    // auto dualSol = restore(dualCanSol, dual.C.size(), dUnbound);
+
+    // Matrix res19 = task.C.T() - dualSol.T() * task.A;
+    // cout << res19;
+    // cout << res19 * x0r;
+
+    vector_t xk = x0r;
+    vector_t xp(3);
+    do {
+        vector_t ak = grad(xk);
+        double bkn = -f(xk) + dot(ak,xk);
+        Matrix Ak(task.A.rows+1,task.A.cols);
+        vector_t bk(task.b.rows+1);
+        for (int i=0; i < task.A.rows; i++) {
+            for (int j=0; j < task.A.cols; j++) {
+                Ak(i,j) = task.A(i,j);
+            }
+        }
+        for (int j=0; j < task.A.cols; j++) {
+            Ak(task.A.rows,j) = -ak[j];
+        }
+        for (int j=0; j < task.b.rows; j++) {
+            bk[j] = -task.b[j];
+        }
+        bk[task.b.rows] = bkn;
+        task.A = Ak;
+        task.b = bk;
+        xp = xk;
+        noneq.insert(task.A.rows-1);
+
+        task_t can = genCanon(task,unbound,noneq,extrem::min);
+        auto initk = initBasic(can);
+        auto xkraw = simplex(can,initk,{});
+        xk = restore(xkraw,can.C.size(), unbound);
+    } while (norm(xk - xp) > 1e-3);
     return 0;
+}
+
+std::tuple<task_t, set<int>, set<int>> read(string filename) {
+    fstream in(filename);
+    int height, width;
+    string line;
+    // sizes
+    getline(in, line);
+    sscanf(line.c_str(), "%d,%d\n", &height, &width);
+    vector_t C(width);
+    Matrix A(width, height);
+    vector_t b(height);
+    // target function
+    getline(in, line);
+    auto funCoefs = split(line, ',');
+    transform(funCoefs.begin(), funCoefs.end(), C.begin(), [](string& str) { return stod(str); });
+    // reading coefs
+    set<int> noneqIndices;
+    for (int i = 0; i < height; i++) {
+        getline(in, line);
+        auto words = split(line, ',');
+        transform(words.begin(), words.begin() + width, A.begin() + i * width, [](string& str) { return stod(str); });
+        b[i] = stod(words[width + 1]);
+        if (words[width] != "EQ") {
+            noneqIndices.insert(i);
+            if (words[width] == "LT") {
+                transform(A.begin() + i * width, A.begin() + (i + 1) * width, A.begin() + i * width,
+                          [](double d) { return -1 * d; });
+                b[i] *= -1;
+            }
+        }
+    }
+    // unbounded variables
+    getline(in, line);
+    auto words = split(line, ',');
+    set<int> unboundIndices;
+    transform(words.begin(), words.end(), inserter(unboundIndices, unboundIndices.end()),
+              [](string& str) { return stoi(str) - 1; });
+    // I almost forgot i store in column-major, so consecutive areas are columns, not rows
+    return {{C, A.T(), b}, unboundIndices, noneqIndices};
+}
+
+vector<string> split(string line, char sep) {
+    stringstream iss(line);
+    vector<string> ret;
+    for (string word; getline(iss, word, sep);) {
+        ret.push_back(word);
+    }
+    return ret;
+}
+
+vector_t restore(vector_t& xCan, int size, set<int>& unbound) {
+    vector_t res(size);
+    copy(xCan.begin(), xCan.begin() + size, res.begin());
+    for (int i : unbound) {
+        int ord = distance(unbound.begin(), find(unbound.begin(), unbound.end(), i));
+        if (xCan[size + ord] > 0) {
+            res[i] -= xCan[size + ord];
+        }
+    }
+    return res;
 }
---
+++ simplex.cpp
@@ -0,0 +1,376 @@
+#include "simplex.h"
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+double eps = 1e-8;
+double eps0 = 1e-13;
+struct {
+    set<int> supSet;
+    vector<bool> mask;
+    bool first;
+    set<int> next() {
+        if (first) {
+            first = !first;
+        } else {
+            if (!prev_permutation(mask.begin(), mask.end())) {
+                return {};
+            }
+        }
+        set<int> perm;
+        auto it = supSet.begin();
+        for (int i = 0; i < mask.size(); i++) {
+            if (mask[i]) {
+                perm.insert(*it);
+            }
+            it = std::next(it);
+        }
+        return perm;
+    }
+    void reset(set<int> L, int size) {
+        supSet = L;
+        mask.resize(0);
+        mask.resize(size, true);
+        mask.resize(supSet.size(), false);
+        first = true;
+    }
+} generator;
+
+void setPrint(set<int> N) {
+    cout << "{";
+    for (int i : N) {
+        cout << " " << i;
+    }
+    cout << "}\n";
+}
+void genTest() {
+    generator.reset({1, 2, 3, 4, 5}, 3);
+    set<int> perm = generator.next();
+    while (perm != set<int>{}) {
+        setPrint(perm);
+        perm = generator.next();
+    }
+}
+void trim(Matrix& v) {
+    for (auto& el : v) {
+        if (abs(el) < eps0) {
+            el = 0;
+        }
+    }
+}
+
+// should not alter initial objects
+set<int> merge(set<int> N, set<int> L) {
+    N.merge(L);
+    return N;
+}
+set<int> fill(Matrix& A, set<int> N);
+Matrix inverse(Matrix A);
+void set_filter(const set<int>& supSet, set<int>& resSet, const function<bool(int)>& pred) {
+    copy_if(supSet.begin(), supSet.end(), inserter(resSet, resSet.end()), pred);
+}
+
+vector_t simplex(task_t task, vector_t x0, set<int> Nk0) {
+    int m = task.A.rows;
+    int n = task.A.cols;
+    set<int> N; // so I can replace most of for loops with set_filter
+    for (int i = 0; i < n; i++) {
+        N.insert(i);
+    }
+    set<int> Nkp;
+    set<int> Nk;
+    vector_t xk = x0;
+    Matrix B = Matrix::eyes(m); // A^-1
+    // 1.
+    set_filter(N, Nkp, [&](int i) { return (xk[i] != 0); });
+    set<int> Lkp;
+    set_filter(N, Lkp, [&](int i) { return (Nkp.find(i) == Nkp.end()); });
+    generator.reset(Lkp, m - Nkp.size());
+    if (Nk0 != set<int>{}) {
+        Nk = Nk0;
+    } else {
+        if (Nkp.size() < m) {
+            Nk = fill(task.A, Nkp);
+        } else {
+            Nk = Nkp;
+        }
+    }
+    B = inverse(task.A[Nk]);
+    // 2.
+    while (true) {
+        // Lk = N \ Nk (not Nkp!)
+        set<int> Lk;
+        set_filter(Lkp, Lk, [&](int i) { return (Nk.find(i) == Nk.end()); });
+        // whole vector to avoid index confusion like in 3.b
+        vector_t dk = task.C.T() - task.C.T()[Nk] * B * task.A;
+        trim(dk);
+        // cout << "\nIter xk: " << xk.T();
+        // setPrint(Nk);
+        // cout << "dk: " << dk.T();
+        // 3.a
+        auto it = find_if(Lk.begin(), Lk.end(), [&dk](int j) { return (dk[j] < 0); });
+        // dk >= 0
+        if (it == Lk.end()) {
+            return xk;
+        }
+        int jk = *it;
+        // 3.b
+        vector_t uknk = B * task.A[{jk}];
+        trim(uknk);
+        vector_t uk(n);
+        // copy using indices I need
+        int i = 0;
+        for (int index : Nk) {
+            uk[index] = uknk[i++];
+        }
+        uk[jk] = -1; // rest is 0 by construction
+        // cout << "uk: " << uk.T();
+        // 4.a
+        set<int> P;
+        // equivalent of P = { i in N | uk[i] > 0}
+        set_filter(Nk, P, [&](int i) { return uk[i] > 0; });
+        if (P.size() == 0) {
+            throw logic_error("no lower bound");
+        }
+        // 4.b
+        int ik = *P.begin();
+        double thk = xk[ik] / uk[ik];
+        for (int i : P) {
+            if (xk[i] / uk[i] < thk) {
+                ik = i;
+                thk = xk[i] / uk[i];
+            }
+        }
+        xk = xk - uk * thk;
+        // to prevent -0. Limits maximum precision, but should prevent many bugs
+        trim(xk);
+        // cout << "theta: " << thk << ", C: " << dot(task.C, xk) << ", x_n: " << xk.T();
+        // 5
+        if (abs(thk) > eps0) { // thk != 0
+            // Matrix F = Matrix::eyes(m);
+            // // F dimensions are m,m. So it's possible to go out of bounds
+            // int dist = distance(Nk.begin(), find(Nk.begin(), Nk.end(), ik));
+            // double div = uk[ik];
+            // for (int i = 0; i < m; i++) {
+            //     if (i != dist) {
+            //         F(i, dist) = -uknk[i] / div;
+            //     } else {
+            //         F(i, i) = 1 / div;
+            //     }
+            //     it++;
+            // }
+            // B = F * B;
+            // recalculating sets
+            Nk.erase(ik);
+            Nk.insert(jk);
+            Lk.erase(jk);
+            Lk.insert(ik);
+
+            Nkp.clear();
+            set_filter(N, Nkp, [&](int i) { return xk[i] > 0; });
+            Lkp.clear();
+            set_filter(N, Lkp, [&](int i) { return xk[i] == 0; });
+            // using F can swap indices (A[Nk] is always in ascending order, which is not guaranteed with F), which
+            // leads to incorrect answers
+            B = inverse(task.A[Nk]);
+            generator.reset(Lk, m - Nkp.size());
+        } else {
+            Nk = fill(task.A, Nkp);
+            auto Ak = task.A[Nk];
+            B = inverse(Ak);
+        }
+    }
+}
+
+// finding starting basic vector
+vector_t initBasic(task_t task) {
+    int n = task.A.cols;
+    int m = task.A.rows;
+    vector_t secC(n + m);
+    for (int i = 0; i < m; i++) {
+        secC[n + i] = 1;
+    }
+    Matrix secA(m, n + m);
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            secA(i, j) = task.A(i, j);
+        }
+        secA(i, n + i) = 1;
+    }
+    vector_t x0(n + m);
+    for (int i = 0; i < m; i++) {
+        if (task.b[i] >= 0) {
+            x0[n + i] = task.b[i];
+        } else {
+            task.b[i] *= -1;
+            x0[n + i] = task.b[i];
+            for (int j = 0; j < n; j++) {
+                secA(i, j) *= -1;
+            }
+        }
+    }
+    set<int> Nk0;
+    for (int i = 0; i < m; i++) {
+        Nk0.insert(i + n);
+    }
+    trim(x0);
+    vector_t res = simplex({secC, secA, task.b}, x0, Nk0);
+    for (int i = 0; i < m; i++) {
+        if (res[n + i] > 0 && abs(res[n + i]) > 3e-11) {
+            cout << res.T();
+            throw logic_error("Empty solution space");
+        }
+    }
+    vector_t xInit(n);
+    copy(res.begin(), res.begin() + n, xInit.begin());
+    return xInit;
+}
+
+set<int> fill(Matrix& A, set<int> Np) {
+    // initial fill, using leftmost vectors
+    set<int> perm = generator.next();
+    // no exact comparison for doubles, should be det == 0
+    // std::prev_permutation is used to generate sequences, so there are no repeats
+    double dcur = det(A[merge(Np, perm)]);
+    while (isnan(dcur) || abs(dcur) < eps) {
+        perm = generator.next();
+        if (perm == set<int>{}) {
+            throw logic_error("looping");
+        }
+        dcur = det(A[merge(Np, perm)]);
+    }
+
+    return merge(Np, perm);
+}
+
+Matrix inverse(Matrix A) {
+    auto [Qt, R] = Matrix::QtRdecomp(A);
+    return Matrix::RInverse(R) * Qt;
+}
+
+// requires det!=0 due to gauss
+Matrix solve(const Matrix& A,const Matrix& b) {
+    int m = A.rows;
+    auto [Qt, R] = Matrix::QtRdecomp(A);
+    auto bq = Qt * b;
+    // gaussBack
+    vector_t res(m);
+    for (int i = m - 1; i >= 0; i--) {
+        double s = 0;
+        for (int k = i + 1; k < m; k++) {
+            s += R(i, k) * res[k];
+        }
+        double x = (bq(i, 0) - s) / R(i, i);
+        res[i] = x;
+    }
+    return res;
+}
+
+// no requirements for A other than squareness, it can always be transformed
+double det(const Matrix& A) {
+    if (A.cols != A.rows) {
+        throw std::invalid_argument("determinant is not defined for non-square matrices");
+    }
+    auto [Qt, R] = Matrix::QtRdecomp(A);
+    double det = 1;
+    for (int i = 0; i < R.rows; i++) {
+        det *= R(i, i);
+    }
+    return det;
+}
+
+vector_t enumerate(task_t task) {
+    int m = task.A.rows;
+    int n = task.A.cols;
+    vector_t x(n);
+    bool first = true;
+    // just build all matrices and test solutions
+    set<int> N;
+    for (int i = 0; i < n; i++) {
+        N.insert(i);
+    }
+    generator.reset(N, m);
+    set<int> Nk = generator.next();
+    // final combination is empty
+    while (Nk != set<int>{}) {
+        if (abs(det(task.A[Nk])) > eps) {
+            // x_t is only using m indices, while n are needed for sizes to match
+            vector_t x_t = solve(task.A[Nk], task.b);
+            trim(x_t);
+            if (!(x_t >= 0)) {
+                Nk = generator.next();
+                continue;
+            }
+            vector_t x_tFull(n);
+
+            auto it = Nk.begin();
+            for (int i = 0; i < m; i++) {
+                x_tFull[*it] = x_t(i, 0);
+                it++;
+            }
+            // cout << x_tFull.T();
+            // compiler doesn't know it's 1x1
+            if (!first) {
+                if (dot(task.C, x_tFull) > dot(task.C, x)) {
+                    copy(x_tFull.begin(), x_tFull.end(), x.begin());
+                }
+            } else {
+                copy(x_tFull.begin(), x_tFull.end(), x.begin());
+                first = false;
+            }
+        }
+        Nk = generator.next();
+    }
+    return x;
+}
+
+std::tuple<task_t, set<int>, set<int>> genDual(task_t task, set<int>& unbound, set<int>& noneq) {
+    Matrix A(task.A.T());
+    vector_t b = task.C;
+    vector_t C = task.b;
+    set<int> dualUnbound;
+    set<int> dualNoneq;
+    // x_i >=0 => A^T[i] y <= C[i]
+    for (int i = 0; i < b.rows; i++) {
+        // x_i >= 0
+        if (unbound.find(i) == unbound.end()) {
+            dualNoneq.insert(i);
+        }
+    }
+    // A[i] x >= b[i] => y_i >= 0
+    for (int i = 0; i < C.rows; i++) {
+        // A[i] x = b[i]
+        if (noneq.find(i) == noneq.end()) {
+            dualUnbound.insert(i);
+        }
+    }
+    return {{C, A, b}, dualUnbound, dualNoneq};
+}
+
+task_t genCanon(task_t task, set<int>& unbound, set<int>& noneq, int sign) {
+    task_t& baseline = task;
+    int size = baseline.A.cols;
+
+    Matrix A(baseline.A.rows, size + unbound.size() + noneq.size());
+    // due to way matrices are stored, it will copy first n columns, which is exactly what I want
+    copy(baseline.A.begin(), baseline.A.end(), A.begin());
+    // x_i = x'_i - v_i, x_'i >=0, v_i >=0
+    vector_t C = baseline.C;
+    int col = size;
+    for (int i : unbound) {
+        C.push_back(-C[i]);
+        for (int j = 0; j < A.rows; j++) {
+            A(j, col) = -A(j, i);
+        }
+        col++;
+    }
+    // a >= b => a - w = b, w >=0 if sign = -1
+    // or a <= b => a + w = b, w >=0 if sign = 1
+    col = size + unbound.size();
+    for (int i : noneq) {
+        C.push_back(0);
+        A(i, col++) = sign;
+    }
+    return {C, A, baseline.b};
+}
